20451a29e839ea531f9f7567c198309f9e4afd25
AmirMahdi-khpv <amirmahdi.khpv@gmail.com>
2024-08-28 01:42:58 +0330

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "0f55bac5-a33c-4f51-84ce-2582116b2800";
+export const MODAL_CLOSE_SEC = 2.5;
+
+// API keys
+// main: 4a06e7c4-d9ff-4ceb-9e09-a9bc9e768a15
+// 0f55bac5-a33c-4f51-84ce-2582116b2800
